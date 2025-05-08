e49ac981e5e648b42876f5b8fc9b63bde0dbd91f
caarosanchez <99395188+caarosanchez@users.noreply.github.com>
2025-05-08 13:57:55 +0400

    Add files via upload

---
+++ Paddle.lua
@@ -0,0 +1,32 @@
+--[[ 
+
+	This is a class file for the
+	'Paddle' object in the Pink Pong game.
+
+	'Paddle' represents an object that can move up and down. 
+	Used in the main program to deflect the ball back 
+	toward the opponent.
+
+]]
+
+Paddle = Class{}
+
+function Paddle:init(x, y, width, height)
+	self.x = x
+	self.y = y
+	self.width = width
+	self.height = height
+	self.dy = 0
+end
+
+function Paddle:update(dt)
+	if self.dy < 0 then
+		self.y = math.max(0, self.y + self.dy * dt)
+	else
+		self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+	end
+end
+
+function Paddle:render()
+	love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
